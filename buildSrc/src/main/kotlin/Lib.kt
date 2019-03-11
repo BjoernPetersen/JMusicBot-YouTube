6c1ec4944ffc8868dcc948271caf98672327a0ff
Björn Petersen <pheasn@gmail.com>
2019-03-11 19:02:57 +0100

    Depend on MusicBot 0.19.0 release

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -1,7 +1,7 @@
 object Lib {
     const val KOTLIN = Plugin.KOTLIN
 
-    const val MUSICBOT = "0.19.0-SNAPSHOT"
+    const val MUSICBOT = "0.19.0"
     const val YOUTUBE_API = "v3-rev20190128-1.28.0"
 
     const val JUNIT = "5.4.0"
