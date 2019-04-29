a9e1b577d82b12cf90f0df126c86766421da7fd8
Björn Petersen <pheasn@gmail.com>
2019-04-29 14:06:54 +0200

    Update YouTube library version

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -2,7 +2,7 @@ object Lib {
     const val KOTLIN = Plugin.KOTLIN
 
     const val MUSICBOT = "0.20.0-SNAPSHOT"
-    const val YOUTUBE_API = "v3-rev20190128-1.28.0"
+    const val YOUTUBE_API = "v3-rev20190415-1.28.0"
 
     const val JUNIT = "5.4.2"
 }
